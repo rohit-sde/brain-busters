9a841ad91eea4fcdc037d94feec7f8217eb49935
Rohit Singh <rohitsinghpb4@gmail.com>
2024-12-28 16:56:55 +0530

    setup roots for nameing player page

--- src/Pages/PlayerNames/PlayerNames.tsx
+++ src/Pages/PlayerNames/PlayerNames.tsx
@@ -1,14 +1,26 @@
+import "./PlayerNames.css";
 import value from "../../Store/Store.ts";
-import Themer from "../../Components/ThemeProvider/Themer.tsx";
+import InfoForm from "../../Components/InfoCollectform/InfoForm.tsx";
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
 
 const PlayerNames = () => {
   console.log(value.getState().About.NoOfPlayers);
   console.log(value.getState().Theme);
+  const NoOfPlayer = value.getState().About.NoOfPlayers;
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (NoOfPlayer === 0) {
+      navigate(-1);
+    }
+  }, []);
   return (
-    <>
-      <div>Hello, There is Nameing Section</div>
-      <Themer></Themer>
-    </>
+    <div className="outterLayer">
+      <InfoForm />
+      <button className="LetGoBtn">
+        Let's Go <span>➙</span>
+      </button>
+    </div>
   );
 };
 
