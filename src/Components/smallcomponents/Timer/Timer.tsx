cbc8ffeb407c445cfa21009a338d49cc04787639
Rohit Singh <rohitsinghpb4@gmail.com>
2025-01-16 17:51:58 +0530

    timer is added

---
+++ src/Components/smallcomponents/Timer/Timer.tsx
@@ -0,0 +1,18 @@
+import "./Timer.css";
+
+const Timer = ({ Size }) => {
+  // console.log(Size);
+  return (
+    <div className="timer">
+      {Size > 800 && <span>05 : 21</span>}
+      {Size < 800 && (
+        <span>
+          <div>05</div>
+          <div>21</div>
+        </span>
+      )}
+    </div>
+  );
+};
+
+export default Timer;
