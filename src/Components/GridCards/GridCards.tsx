6773e451ac519bb5d64927c3c97065e4538935bf
Rohit Singh <rohitsinghpb4@gmail.com>
2025-01-18 23:08:52 +0530

    logic and grid pattern added

--- src/Components/GridCards/GridCards.tsx
+++ src/Components/GridCards/GridCards.tsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
 import value from "../../Store/Store";
 import "./GridCards.css";
+import handleCardsDetails from "../../funcs/CardsGenerator";
 
 const GridCards = () => {
   const [grid, setgrid] = useState<{
     TypeOfCards: string;
     GridSize: number;
-  }>();
+  }>(value.getState().Board.Cards);
+  const [cardsArray, setCardsArray] = useState<string[] | number[]>([]);
   useEffect(() => {
     const unsubscribe = value.subscribe(() => {
       const updatedGrid = value.getState().Board.Cards;
@@ -14,25 +16,21 @@ const GridCards = () => {
     });
     setgrid({ ...value.getState().Board.Cards });
 
+    setCardsArray(() => handleCardsDetails(grid?.TypeOfCards, grid?.GridSize));
     return () => {
       unsubscribe();
     };
   }, []);
   return (
-    <div className="CardsGrid">
-      {Array(grid?.GridSize)
-        .fill("null")
-        .map((card, i) => {
-          return (
-            <span key={i}>
-              {Array(grid?.GridSize)
-                .fill("null")
-                .map((card, k) => {
-                  return <span key={k}>{card}</span>;
-                })}
-            </span>
-          );
-        })}
+    <div
+      className="CardsGrid"
+      style={{
+        gridTemplateColumns: `repeat(${grid.GridSize / 2},minmax(0,1fr))`,
+      }}
+    >
+      {cardsArray.map((val, i) => {
+        return <span key={i}>{val}</span>;
+      })}
     </div>
   );
 };
