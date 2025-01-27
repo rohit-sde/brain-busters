f1e953d3ba107189bdc2f008aede43e64f3db720
Rohit Singh <rohitsinghpb4@gmail.com>
2025-01-27 16:27:52 +0530

    inputs handling logic changed

--- src/Components/smallcomponents/InputSection/InputSection.tsx
+++ src/Components/smallcomponents/InputSection/InputSection.tsx
@@ -1,28 +1,42 @@
 import { useDispatch, useSelector } from "react-redux";
 import "./InputSection.css";
-import { SetCards } from "../../../Store/AboutGame";
+import { SetCards, SetPlayerDetails } from "../../../Store/AboutGame";
 import { useState } from "react";
 
 const InputSection = () => {
-  const timeforplayers = useSelector((v) =>
-    v.About.PlayersDetails.map((ele: object) => ele.time)
-  );
+  const playersarray = useSelector((v) => v.About.PlayersDetails);
+  const playerstime = playersarray.map((ele: object) => ele.time);
   const GridNum = useSelector((v) => v.Board.Cards.GridSize);
-  const [time, setTime] = useState(timeforplayers);
+  const [time, setTime] = useState(playerstime);
   const dispatch = useDispatch();
 
-  function handleGridSize(e: any) {
-    dispatch(SetCards(Number(e.target.value)));
+  function handleInputs(e: any, type) {
+    let value = parseFloat(e.target.value);
+    let min = parseFloat(e.target.getAttribute("min"));
+    let max = parseFloat(e.target.getAttribute("max"));
+
+    if (value < min) {
+      e.target.value = min;
+    } else if (value > max) {
+      e.target.value = max;
+    }
+
+    if (type == "grid") {
+      dispatch(SetCards(Number(e.target.value)));
+    } else if (type == "min") {
+      setTime((prev) => ({ ...prev, min: e.target.value }));
+    } else if (type == "sec") {
+      setTime((prev) => ({ ...prev, sec: e.target.value }));
+    }
   }
 
-  function handleMin(e: object) {
-    setTime((prev) => ({ ...prev, min: e.target.value }));
+  function handleSetButton() {
+    playersarray.forEach((ele: object, i: number) => ({
+      ...ele,
+      time: time[i],
+    }));
+    dispatch(SetPlayerDetails(playersarray));
   }
-
-  function handleSec(e: object) {
-    setTime((prev) => ({ ...prev, sec: e.target.value }));
-  }
-
   return (
     <div className="InputSectionWrapper">
       <span className="GridSize">
@@ -33,32 +47,32 @@ const InputSection = () => {
           value={GridNum}
           max={7}
           min={2}
-          onChange={handleGridSize}
+          onChange={(e) => handleInputs(e, "grid")}
         />
       </span>
       <span className="SetTime">
         <span>
-          <label htmlFor="minutes">Min :</label>
+          <label htmlFor="minutes">Min :-</label>
           <input
             type="number"
-            value={time[0].min}
-            onChange={handleMin}
+            defaultValue={time[0]?.min}
+            onChange={(e) => handleInputs(e, "min")}
             min={0}
             max={10}
           />
         </span>
         <span>
-          <label htmlFor="seconds">Sec :</label>
+          <label htmlFor="seconds">Sec :-</label>
           <input
             type="number"
-            value={time[0].sec}
-            onChange={handleSec}
+            defaultValue={time[0]?.sec}
+            onChange={(e) => handleInputs(e, "sec")}
             min={0}
             max={60}
           />
         </span>
       </span>
-      <button>Set</button>
+      <button onClick={handleSetButton}>Set</button>
     </div>
   );
 };
