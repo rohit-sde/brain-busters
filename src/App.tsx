076a2bdffee3cd91b5d82e3f2ab9fe7eb3b787ca
Rohit Singh <rohitsinghpb4@gmail.com>
2025-01-30 23:07:56 +0530

    connected custom themes Componet

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,15 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import PlayerNames from "./Pages/PlayerNames/PlayerNames.tsx";
 import GameBoard from "./Pages/GameBoard/GameBoard.tsx";
 import Themer from "./Components/smallcomponents/ThemeProvider/Themer.tsx";
+// import CustomTheme from "./Components/smallcomponents/customThemer/CustomTheme.tsx";
 
 function App() {
   return (
     <>
-      <Themer />
+      <span className="themesWrapper">
+        <Themer />
+        {/* <CustomTheme /> */}
+      </span>
       <div className="wrapper">
         <BrowserRouter>
           <Routes>
