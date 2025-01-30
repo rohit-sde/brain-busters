0a4ab146a104199d4c880c45cdcf056859d78dad
Rohit Singh <rohitsinghpb4@gmail.com>
2025-01-30 23:08:54 +0530

    added colors in consts

---
+++ src/Components/smallcomponents/customThemer/const/darkColors.ts
@@ -0,0 +1,15 @@
+interface colorsSet {
+  theme: string;
+  colors: string[];
+}
+
+export const darkColors: colorsSet = {
+  theme: "dark",
+  colors: [
+    "#8a8a60,#b8b07b",
+    "#4c8a94,#8d9d9f",
+    "#6e1f32,#9b6b4c",
+    "#3b7373,#cf7e5e",
+    "#5e3f74,#9a7da6",
+  ],
+};
---
+++ src/Components/smallcomponents/customThemer/const/lightColors.ts
@@ -0,0 +1,15 @@
+interface colorsSet {
+  theme: string;
+  colors: string[];
+}
+
+export const lightColors: colorsSet = {
+  theme: "light",
+  colors: [
+    "#a89e47,#f2a7b2",
+    "#7e702a,#9f9a6d",
+    "#e96f35,#3e8e41",
+    "#5979b1,#a2c8e3",
+    "#c49f68,#db5c4d",
+  ],
+};
