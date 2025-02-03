5d97cc43bcb02bbc6ee4a11bd5d8ed7f3b7ea426
Rohit Singh <rohitsinghpb4@gmail.com>
2025-02-04 00:59:15 +0530

    Timer is set dynamic

--- src/Components/smallcomponents/Timer/Timer.tsx
+++ src/Components/smallcomponents/Timer/Timer.tsx
@@ -1,14 +1,23 @@
+import { useSelector } from "react-redux";
 import "./Timer.css";
 
 const Timer = ({ Size = 801 }: { Size?: number }) => {
-  // console.log(Size);
+  const CurrentPlayer = useSelector((val) => val.About.CurrentTurn - 1);
+  const Time = useSelector(
+    (val) => val.About.PlayersDetails[CurrentPlayer].time
+  );
+  console.log(Time);
   return (
     <div className="timer">
-      {Size > 800 && <span>05 : 21</span>}
+      {Size > 800 && (
+        <span>
+          0{Time?.min} : {Time?.sec}
+        </span>
+      )}
       {Size < 800 && (
         <span>
-          <div>05</div>
-          <div>21</div>
+          <div>0{Time?.min}</div>
+          <div>{Time?.sec}</div>
         </span>
       )}
     </div>
