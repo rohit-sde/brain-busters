eabcb9b0f0c5748c47b6c64661b51cbde9f15b54
Rohit Singh <rohitsinghpb4@gmail.com>
2025-02-03 22:58:21 +0530

    scroll bar added

--- src/Components/LiveRanking/LiveRanking.tsx
+++ src/Components/LiveRanking/LiveRanking.tsx
@@ -1,9 +1,11 @@
 import "./LiveRanking.css";
 import value from "../../Store/Store";
 import { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import { SetScore } from "../../Store/AboutGame";
 import Timer from "../smallcomponents/Timer/Timer";
+import { emojis } from "../../Pages/PlayerNames/conts/conts";
+import styled from "styled-components";
 
 interface playersDetails {
   id: number;
@@ -18,41 +20,31 @@ interface playersDetails {
 const LiveRanking = () => {
   const [playersDetail, setPlayersDetail] = useState<playersDetails[]>([]);
   const [WindowSize, setWindowSize] = useState(window.innerWidth);
-  const emojiArray = ["😊", "😍", "😎", "🥺", "😂", "🤔", "🙌", "💥", "🌟"];
-  // console.log(WindowSize);
+  const CurrentDetails = useSelector((val) => val.About.PlayersDetails);
+
+  useEffect(() => {
+    setPlayersDetail([...CurrentDetails]);
+  }, [CurrentDetails]);
 
   useEffect(() => {
     function handleResize() {
       setWindowSize(window.innerWidth);
     }
     window.addEventListener("resize", handleResize);
-    const unsubscribe = value.subscribe(() => {
-      const updatedDetails = value.getState().About.PlayersDetails;
-      setPlayersDetail([...updatedDetails]);
-    });
-    setPlayersDetail([...value.getState().About.PlayersDetails]);
 
     return () => {
-      unsubscribe();
       window.removeEventListener("resize", handleResize);
     };
   }, []);
 
   // console.log(playersDetail, "playersDetail");
-  const sortedPlayers = [...playersDetail].sort((a, b) => b.Score - a.Score);
+  const sortedPlayers = playersDetail?.sort((a, b) => b.Score - a.Score);
   function getRank(position: number) {
     if (position == 1) return "st";
     if (position == 2) return "nd";
     if (position == 3) return "rd";
     return "th";
   }
-  // console.log(sortedPlayers, "sortedPlayers");
-
-  // console.log("-------------------");
-  // console.log(sortedPlayers[0]?.Score, "Player 0");
-  // console.log(sortedPlayers[1]?.Score, "Player 1");
-  // console.log(sortedPlayers[2]?.Score, "Player 2");
-  // console.log(sortedPlayers[3]?.Score, "Player 3");
   return (
     <div
       className="container"
@@ -68,15 +60,17 @@ const LiveRanking = () => {
                   ? "Solo"
                   : `${position}${getRank(position)}`)}
               <span
-                className={`num${sortedPlayers.length == 1 ? "" : position}`}
+                className={`playerImage num${
+                  sortedPlayers.length == 1 ? "" : position
+                }`}
               >
-                {emojiArray.includes(player.character) ? (
+                {emojis.includes(player.character) ? (
                   <span>{player.character}</span>
                 ) : (
                   <img src={`${player.character}`} />
                 )}
               </span>
-              {WindowSize > 860 && player.playerName}
+              <NameWrapper>{WindowSize > 860 && player.playerName}</NameWrapper>
             </div>
           );
         })}
@@ -87,3 +81,34 @@ const LiveRanking = () => {
 };
 
 export default LiveRanking;
+
+const NameWrapper = styled.span`
+  text-wrap: nowrap;
+`;
+
+// console.log(sortedPlayers, "sortedPlayers");
+
+// console.log("-------------------");
+// console.log(sortedPlayers[0]?.Score, "Player 0");
+// console.log(sortedPlayers[1]?.Score, "Player 1");
+// console.log(sortedPlayers[2]?.Score, "Player 2");
+// console.log(sortedPlayers[3]?.Score, "Player 3");
+
+// console.log(WindowSize);
+
+// useEffect(() => {
+//   function handleResize() {
+//     setWindowSize(window.innerWidth);
+//   }
+//   window.addEventListener("resize", handleResize);
+//   const unsubscribe = value.subscribe(() => {
+//     const updatedDetails = value.getState().About.PlayersDetails;
+//     setPlayersDetail([...updatedDetails]);
+//   });
+//   setPlayersDetail([...value.getState().About.PlayersDetails]);
+
+//   return () => {
+//     unsubscribe();
+//     window.removeEventListener("resize", handleResize);
+//   };
+// }, []);
