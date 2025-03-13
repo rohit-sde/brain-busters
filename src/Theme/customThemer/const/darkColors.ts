eb7f3d949ea43e3a096f1fae582f48fb5fa4a938
Rohit Singh <rohitsinghpb4@gmail.com>
2025-03-13 14:26:58 +0530

    color sets structure changed

--- src/Theme/customThemer/const/darkColors.ts
+++ src/Theme/customThemer/const/darkColors.ts
@@ -1,15 +1,19 @@
+interface obj {
+  cName: string;
+  color: string;
+}
 interface colorsSet {
   theme: string;
-  colors: string[];
+  colors: obj[];
 }
 
 export const darkColors: colorsSet = {
   theme: "dark",
   colors: [
-    "#8a8a60,#b8b07b",
-    "#4c8a94,#8d9d9f",
-    "#6e1f32,#9b6b4c",
-    "#3b7373,#cf7e5e",
-    "#5e3f74,#9a7da6",
+    { cName: "classicminimal", color: "#8a8a60,#b8b07b" },
+    { cName: "deepocean", color: "#4c8a94,#8d9d9f" },
+    { cName: "richburgundy", color: "#6e1f32,#9b6b4c" },
+    { cName: "tealCopper", color: "#3b7373,#cf7e5e" },
+    { cName: "mysticpurple", color: "#5e3f74,#9a7da6" },
   ],
 };
--- src/Theme/customThemer/const/lightColors.ts
+++ src/Theme/customThemer/const/lightColors.ts
@@ -1,15 +1,19 @@
+interface obj {
+  cName: string;
+  color: string;
+}
 interface colorsSet {
   theme: string;
-  colors: string[];
+  colors: obj[];
 }
 
 export const lightColors: colorsSet = {
   theme: "light",
   colors: [
-    "#a89e47,#f2a7b2",
-    "#7e702a,#9f9a6d",
-    "#e96f35,#3e8e41",
-    "#5979b1,#a2c8e3",
-    "#c49f68,#db5c4d",
+    { cName: "softpastels", color: "#a89e47,#f2a7b2" },
+    { cName: "earthytones", color: "#7e702a,#9f9a6d" },
+    { cName: "vibrantcontrast", color: "#e96f35,#3e8e41" },
+    { cName: "mutedblues", color: "#5979b1,#a2c8e3" },
+    { cName: "warmsunset", color: "#c49f68,#db5c4d" },
   ],
 };
