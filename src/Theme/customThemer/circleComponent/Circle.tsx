7a40595ca410d6fea3c9f0e2c3174b1efb2bd68a
Rohit Singh <rohitsinghpb4@gmail.com>
2025-03-13 14:26:31 +0530

    circle component handled

--- src/Theme/customThemer/circleComponent/Circle.tsx
+++ src/Theme/customThemer/circleComponent/Circle.tsx
@@ -1,13 +1,34 @@
-const Circle = ({ colors }: { colors?: string }) => {
-  const styleCircle = {
-    height: "23px",
-    width: "23px",
-    borderRadius: "16px",
-    filter: "brightness(1.6)",
-    cursor: "pointer",
-    background: `linear-gradient(${colors})`,
-  };
-  return <div style={styleCircle}></div>;
+import styled from "styled-components";
+
+const Circle = ({
+  colors,
+  func,
+  isActive,
+}: {
+  colors?: string;
+  isActive: boolean;
+}) => {
+  return (
+    <Circlediv
+      colors={colors || "red,yellow"}
+      onClick={func}
+      active={isActive}
+    ></Circlediv>
+  );
 };
+
+const Circlediv = styled.div<{ colors: string; active: boolean }>`
+  height: 23px;
+  width: 23px;
+  border-radius: 16px;
+  filter: brightness(1.6);
+  cursor: pointer;
+  background: linear-gradient(${(props) => props.colors});
+  transition: all 0.4s;
+  transform: ${(props) => (props.active ? "scale3d(1.5, 1.5, 1.5)" : "none")};
+  &:hover {
+    transform: scale3d(1.5, 1.5, 1.5);
+  }
+`;
 
 export default Circle;
